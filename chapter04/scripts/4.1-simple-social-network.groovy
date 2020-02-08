0848b86cfda7cc23cd5f5d241ff7a6aec80eaaea
Dave <dave@bechberger.com>
2020-02-08 11:43:56 -0900

    Fixed scripts

--- chapter04/scripts/4.1-simple-social-network.groovy
+++ chapter04/scripts/4.1-simple-social-network.groovy
@@ -1,12 +1,28 @@
+//Connects the Gremlin Session to the "remote" Gremlin Server
+//and runs this in a session instead of the default "sessionless" mode
 :remote connect tinkerpop.server conf/remote.yaml session
+//Sets Gremlin Console into "remote" mode so that each command is run on the server
 :remote console
 
+//Remove any existing data to allow this to be rerun
 g.V().drop().iterate()
 
-full_path_and_filename = "/path/to/simple_social_network.json"
+//Adds a person vertex with a name of Dave and saves it to a variable
+dave = g.addV('person').property('first_name', 'Dave').next()
+//Adds a person vertex with a name of Josh and saves it to a variable
+josh = g.addV('person').property('first_name', 'Josh').next()
+//Adds a person vertex with a name of Ted and saves it to a variable
+ted = g.addV('person').property('first_name', 'Ted').next()
+//Adds a person vertex with a name of Hank and saves it to a variable
+hank = g.addV('person').property('first_name', 'Hank').next()
 
-g.io(full_path_and_filename).
-  with(IO.reader,IO.graphson).
-  read().
-  iterate()
-
+//Adds a friends edge between Dave and Ted
+g.addE('friends').from(dave).to(ted).next()
+//Adds a friends edge between Dave and Josh
+g.addE('friends').from(dave).to(josh).next()
+//Adds a friends edge between Dave and Hank
+g.addE('friends').from(dave).to(hank).next()
+//Adds a friends edge between Josh and Hank
+g.addE('friends').from(josh).to(hank).next()
+//Adds a friends edge between Ted and Josh
+g.addE('friends').from(ted).to(josh).next()
